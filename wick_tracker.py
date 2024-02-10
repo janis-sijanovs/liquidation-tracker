c7023746a777240caf3cacc5a0139b566e90d381
janis-sijanovs <janis.sijanovs@gmail.com>
2024-02-10 00:03:56 +0000

    Added messy code for unusually long candles

--- wick_tracker.py
+++ wick_tracker.py
@@ -3,13 +3,18 @@ import asyncio
 import requests
 from requests.exceptions import HTTPError
 from time import sleep
-from datetime import datetime
+from datetime import datetime, timedelta
 from playsound import playsound
 
 BASE_URL = 'https://fapi.binance.com'
 RETRACE_THRESHOLD = 35  # Percentage threshold for retracement
 MIN_CANDLE_PERCENTAGE = 1
+LARGE_CANDLE_PERCENT = 3
 SOUND_FILE = "sounds/wickwickwick.wav"
+LARGE_SOUND_FILE = "sounds/largelarge.wav"
+
+message_history = {}
+large_history = {}
 
 def percentage_diff(high, low):
     return (high - low) * 100 / high
@@ -37,24 +42,105 @@ async def get_candlestick_data(symbol, interval='1m'):
 
 def calculate_retracement(candlestick):
     if not len(candlestick):
-        return 0.0
+        return 0, "", 0
+    
     open_p, high_p, low_p, close_p = map(float, candlestick[1:5])
-    if abs(percentage_diff(high_p, low_p)) < MIN_CANDLE_PERCENTAGE:
-        return 0
+    candle_percent = abs(percentage_diff(high_p, low_p))
+    
     total = high_p - low_p
-    if close_p > open_p:
-        wick = high_p - close_p
-    else:
-        wick = close_p - low_p
-    if not wick:
-        return 0
-    retracement = wick * 100 / total
-    return retracement
 
-def notify(symbol, retracement):
-    message = f'{symbol} Retraced {retracement:.2f}%'
+    if close_p > open_p:
+        upper_shadow = high_p - close_p
+        lower_shadow = open_p - low_p
+        wick = max(upper_shadow, lower_shadow)
+    else:
+        upper_shadow = close_p - low_p
+        lower_shadow = high_p - open_p
+        wick = max(upper_shadow, lower_shadow)
+
+    if not wick:
+        return 0, "", candle_percent
+    
+    retracement = wick * 100 / total
+    direction = "DOWN" if upper_shadow > lower_shadow else "UP"
+    return retracement, direction, candle_percent
+
+
+def check_candle(symbol, candle):
+    retracement, direction, candle_percent = calculate_retracement(candle)
+    if candle_percent > LARGE_CANDLE_PERCENT:
+        ts = candle[0]
+
+        seconds = ts // 1000
+        milliseconds = ts % 1000
+        dt_base = datetime.fromtimestamp(seconds)
+        dt = dt_base + timedelta(milliseconds=milliseconds)
+        dt = dt.replace(microsecond=0)
+
+        notify_large(symbol, dt, candle_percent)
+
+    if retracement >= RETRACE_THRESHOLD and candle_percent > MIN_CANDLE_PERCENTAGE:
+        ts = candle[0]
+
+        seconds = ts // 1000
+        milliseconds = ts % 1000
+        dt_base = datetime.fromtimestamp(seconds)
+        dt = dt_base + timedelta(milliseconds=milliseconds)
+        dt = dt.replace(microsecond=0)
+        
+        notify(symbol, dt, retracement, direction, candle_percent)
+
+def notify_large(symbol, dt, candle_percent):
+    try:
+        if large_history[symbol][0] == dt and large_history[symbol][1] >= candle_percent:
+            return 0
+    except KeyError:
+        pass
+    
+    if candle_percent >2.5:
+        color_percent = '\033[94m'  # ANSI escape code for green color
+    else:
+        color_percent = ''  # Default color
+
+    # Your original message
+        
+    message = f'{dt} \033[35m{symbol}\033[0m Large \033[94m{candle_percent:.2f}%\033[0m Candle'
     print(message)  # Replace with your notification code
+    print()
+    playsound(LARGE_SOUND_FILE)
+
+    if symbol not in large_history.keys() or dt > large_history[symbol][0]:
+        large_history[symbol] = [dt, candle_percent]
+
+def notify(symbol, dt, retracement, direction, candle_percent):
+    try:
+        if message_history[symbol] == dt:
+            return 0
+    except KeyError:
+        pass
+    
+    if direction == 'UP':
+        color_code = '\033[92m'  # ANSI escape code for green color
+    elif direction == 'DOWN':
+        color_code = '\033[91m'  # ANSI escape code for red color
+    else:
+        color_code = ''  # Default color
+    
+    if candle_percent >2.5:
+        color_percent = '\033[94m'  # ANSI escape code for green color
+    else:
+        color_percent = ''  # Default color
+
+    # Your original message
+        
+    message = f'{dt} \033[35m{symbol}\033[0m {color_code}{direction}\033[0m {retracement:.2f}% from {color_percent}{candle_percent:.2f}% \033[0m'
+    print(message)  # Replace with your notification code
+    print()
     playsound(SOUND_FILE)
+
+    
+    if symbol not in message_history.keys() or dt > message_history[symbol]:
+        message_history[symbol] = dt
 
 async def track_all_pairs():
     symbols = get_all_usdt_futures_pairs()
@@ -67,12 +153,8 @@ async def track_all_pairs():
 
         for symbol, candlesticks in zip(symbols, candlesticks_list):
             if len(candlesticks) >= 2:
-                retracement = calculate_retracement(candlesticks[0])
-                if retracement >= RETRACE_THRESHOLD:
-                    notify(symbol, retracement)
-                retracement = calculate_retracement(candlesticks[1])
-                if retracement >= RETRACE_THRESHOLD:
-                    notify(symbol, retracement)
+                for candle in candlesticks:
+                    check_candle(symbol, candle)
 
         await asyncio.sleep(20)  # Check every 5 seconds
 
